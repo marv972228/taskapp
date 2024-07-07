bfc26c9536de400653262a2a86cc10899eb99793
Marvin Mallari <marvinmallari@gmail.com>
2024-07-07 12:54:52 +0000

    move taskapp

--- frontend/src/TaskApp.tsx
+++ frontend/src/TaskApp.tsx
@@ -1,9 +1,8 @@
-import AddTask from './AddTask';
-import TaskList from './TaskList';
-import { TasksProvider } from '../../context/TaskContext';
-import { AppWrapper, Container, Header } from './StyledComponents';
-import CompletedTasksCounter from '../CompletedTasks';
-import CircularProgress from '../CircularProgress';
+import TaskList from './components/Tasks/TaskList';
+import { TasksProvider } from './context/TaskContext';
+import { AppWrapper, Container, Header } from './components/Tasks/StyledComponents';
+import CompletedTasksCounter from './components/CompletedTasks';
+import CircularProgress from './components/CircularProgress';
 
 export default function TaskApp() {
   return (
